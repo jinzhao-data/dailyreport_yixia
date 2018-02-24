09a5ef0a503406286ccb8d0c0bda69ff650ce0e1
jinzhao <zjin2008@126.com>
2018-02-24 11:28:41 +0800

    增加分新老，加启动次数

--- starttype.R
+++ starttype.R
@@ -1,3 +1,4 @@
+#####加载包与连接数据库####
 library(tidyverse)
 library(rpivotTable)
 library(clipr)
@@ -8,18 +9,24 @@ library(stringr)
 library(lubridate)
 con <- dbConnect(RPresto::Presto(), host = "47.93.29.9", port = 8080, user = "yanagishima", 
                  schema = "default", catalog = "hive", source = "yanagishima")
+#####sql####
 res <- dbGetQuery(con, "SELECT dt,
                   PARAMETERS['sdk_from'] AS sdk_from,
+                  PARAMETERS['new_device_tag'] AS usertype,
+                  count(1) as starttimes,
                   count(DISTINCT udid) AS countudid
                   FROM dws_bobo_logs.dws_bobo_sdk_all
                   WHERE dt >= date_format(date_add('day',-8,current_date),'%Y%m%d')
                   AND dt <= date_format(date_add('day',-1,current_date),'%Y%m%d')
                   AND eventid = 'app_start'
                   GROUP BY dt,
-                  PARAMETERS['sdk_from']
+                  PARAMETERS['sdk_from'],
+                  PARAMETERS['new_device_tag'] 
                   ORDER BY dt DESC")
+#####写入excle####
 today <- str_c("/Users/jinzhao/Desktop/data/", "starttype", as.character(Sys.Date()), 
                ".xlsx")
 write.xlsx(res, today)
 dbDisconnect(con)
+#####透视表####
 rpivotTable(res)
